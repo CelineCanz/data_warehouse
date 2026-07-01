19963470dc879ac3844656bd50b0a79eacb939a0
Celine Canziani <celine.canziani@eu.yamaha.com>
2026-07-01 16:51:11 +0200

    last changes

--- pipelines/sap_flows.py
+++ pipelines/sap_flows.py
@@ -9,6 +9,7 @@ from prefect import flow, task
 from config.sap_date import process_sap_datetime
 from config.sql_server import write_df, create_table
 
+
 from ingestion.sap.product.product_full_load import (
     extract_product,
     extract_product_description,
@@ -26,42 +27,28 @@ def prepare_dataframe(df):
 
     df = process_sap_datetime(df)
 
+    cols_to_drop = [
+
+        col for col in df.columns
+
+        if (
+            "__metadata" in col
+            or "__deferred" in col
+        )
+
+    ]
+
     df = df.drop(
-        columns=[
-            "__metadata.id",
-            "__metadata.uri",
-            "__metadata.type"
-        ],
+        columns=cols_to_drop,
         errors="ignore"
     )
 
     return df
 
-
-# PRODUCT
-
-# %%
-@task
-def run_product():
-
-    df = prepare_dataframe(
-        extract_product()
-    )
-
-    create_table(
-        df,
-        "DTL_SAP_Product"
-    )
-
-    return df
-
-
-
-
 @task
 def create_and_load(
     df,
-    table_name,
+    table_name
 ):
 
     create_table(
@@ -75,6 +62,40 @@ def create_and_load(
     )
 
 
+# PRODUCT
+# %%
+@task
+def run_product():
+    return prepare_dataframe(
+        extract_product()
+    )
+
+
+@task
+def run_product_desc():
+    return prepare_dataframe(
+        extract_product_description()
+    )
+
+
+@task
+def run_product_sales_delivery():
+    return prepare_dataframe(
+        extract_product_sales_delivery()
+    )
+
+@task
+def run_billing_header():
+    return prepare_dataframe(
+        load_billing_header()
+    )
+
+@task
+def run_billing_item():
+    return prepare_dataframe(
+        load_billing_item()
+    )
+
 
 @flow(name="SAP Full Load")
 def sap_flow():
@@ -86,14 +107,14 @@ def sap_flow():
         "DTL_SAP_Product"
     )
 
-    product_description = extract_product_description()
+    product_description = run_product_desc()
 
     create_and_load(
         product_description,
         "DTL_SAP_Product_Description"
     )
 
-    product_sales_delivery = extract_product_sales_delivery()
+    product_sales_delivery = run_product_sales_delivery()
 
     create_and_load(
         product_sales_delivery,
@@ -101,14 +122,32 @@ def sap_flow():
     )
 
     
-    billing_header = load_billing_header()
+    billing_header = run_billing_header()
 
     create_and_load(
         billing_header,
         "DTL_SAP_BillingHeader"
     )
 
-    billing_item = load_billing_item()
+    billing_item = run_billing_item()
+
+    create_and_load(
+        billing_item,
+        "DTL_SAP_BillingItem"
+    )
+
+
+@flow(name="SAP Full Load")
+def sap_flow():
+    
+    billing_header = run_billing_header()
+
+    create_and_load(
+        billing_header,
+        "DTL_SAP_BillingHeader"
+    )
+
+    billing_item = run_billing_item()
 
     create_and_load(
         billing_item,
--- pipelines/test_products.py
+++ pipelines/test_products.py
@@ -5,8 +5,9 @@ sys.path.append(r"E:\Python\data_warehouse")
 
 # %%
 from prefect import flow, task
-
-# %%
+from config.sql_server import create_table
+from config.sap_date import process_sap_datetime
+from config.sql_server import write_df_bcp
 
 from ingestion.sap.product.product_full_load import extract_product
 
@@ -16,16 +17,39 @@ from ingestion.sap.billing.billing_full_load import (
 )
 
 
+# FONCTION Let's get the metadata out and prepare dates
+def prepare_dataframe(df):
+
+    df = process_sap_datetime(df)
+
+    df = df.drop(
+        columns=[
+            "__metadata.id",
+            "__metadata.uri",
+            "__metadata.type"
+        ],
+        errors="ignore"
+    )
+
+    return df
+
 
 # PRODUCT
 
+# %%
 @task
 def run_product():
-    print("Running product extraction...")
-    df = extract_product()
-    print(f"Rows loaded: {df.shape}")
-    return df
 
+    df = prepare_dataframe(
+        extract_product()
+    )
+
+    create_table(
+        df,
+        "DTL_SAP_Product"
+    )
+
+    return df
 
 # BILLING HEADER
 
@@ -33,6 +57,7 @@ def run_product():
 def run_billing_header():
     print("Running billing header extraction...")
     df = load_billing_header()
+    df = process_sap_datetime(df)
     print(f"Rows loaded: {df.shape}")
     return df
 
@@ -43,35 +68,57 @@ def run_billing_header():
 def run_billing_item():
     print("Running billing item extraction...")
     df = load_billing_item()
+    df = process_sap_datetime(df)
     print(f"Rows loaded: {df.shape}")
     return df
 
 
 
 
-
+# %%
 @flow(name="SAP Full Load")
 def sap_flow():
 
     product = run_product()
-
     billing_header = run_billing_header()
-
     billing_item = run_billing_item()
 
-    return {
-        "DTL_SAP_Product": product,
-        "DTL_SAP_BillingHeader": billing_header,
-        "DTL_SAP_BillingItem": billing_item,
-    }
+    print(product.shape)
+    print(billing_header.shape)
+    print(billing_item.shape)
 
 
 
 
+
+# %%
 if __name__ == "__main__":
+    sap_flow()
 
-    tables = sap_flow()
 
-    DTL_SAP_Product = tables["DTL_SAP_Product"]
-    DTL_SAP_BillingHeader = tables["DTL_SAP_BillingHeader"]
-    DTL_SAP_BillingItem = tables["DTL_SAP_BillingItem"]
+
+
+# %%
+df = prepare_dataframe(extract_product())
+
+create_table(
+    df,
+    "DTL_SAP_Product"
+)
+
+# %%
+import importlib
+import config.sql_server as sql_server
+
+importlib.reload(sql_server)
+
+from config.sql_server import write_df
+
+df_test = df.head(1000)
+
+write_df(
+    df,
+    table_name="DTL_SAP_Product"
+)
+
+
